9b34d591bce5eda74c6c73874af1d84d94064b36
subhankarroy612 <subhankarroy612@gmail.com>
2023-02-09 18:47:03 +0530

    cors added

--- next_app/src/components/url.js
+++ next_app/src/components/url.js
@@ -1,2 +1,2 @@
-// export const url = 'https://task-manager-xi-pink.vercel.app'
-export const url = 'http://localhost:3000'
+export const url = 'https://task-manager-xi-pink.vercel.app'
+// export const url = 'http://localhost:3000'
